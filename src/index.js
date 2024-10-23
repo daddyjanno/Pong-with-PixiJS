f52a161e82a2024bebec8c73dd3ca84831d84a0b
daddyjanno <jn.drugmand@gmail.com>
2024-10-23 10:43:30 +0200

    basic start

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("Pong with PixiJS");
+import { Application } from 'pixi.js'
+
+console.log('Pong with PixiJS')
+
+const app = new Application()
+await app.init({
+    resizeTo: window,
+})
+document.body.appendChild(app.canvas)
