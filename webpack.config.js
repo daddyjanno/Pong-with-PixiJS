7a04b01a3c783e27b424beabc30d2ae9dc508921
daddyjanno <jn.drugmand@gmail.com>
2024-10-23 10:40:20 +0200

    Pong setup

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 9000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+        ],
+    },
+    mode: 'development',
+}
