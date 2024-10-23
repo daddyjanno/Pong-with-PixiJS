c1b20ccf979eb29df046796c6b43db5bda1ff465
daddyjanno <jn.drugmand@gmail.com>
2024-10-23 11:08:15 +0200

    creating ball and pads

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { Application } from 'pixi.js'
+import { Application, Graphics } from 'pixi.js'
 
 console.log('Pong with PixiJS')
 
@@ -8,3 +8,34 @@ await app.init({
     antialias: true,
 })
 document.body.appendChild(app.canvas)
+
+function createGameParts() {
+    const ball = createBall(5, 0xffffff)
+    ball.x = app.screen.width / 2
+    ball.y = app.screen.height / 2
+    app.stage.addChild(ball)
+
+    const playerPad = createPad(10, 50, 0xffffff)
+    playerPad.x = 50
+    playerPad.y = app.screen.height / 2
+    app.stage.addChild(playerPad)
+
+    const botPad = createPad(10, 50, 0xffffff)
+    botPad.x = app.screen.width - 50
+    botPad.y = app.screen.height / 2
+    app.stage.addChild(botPad)
+}
+
+function createBall(radius, color) {
+    const ball = new Graphics().circle(0, 0, radius).fill(color)
+    return ball
+}
+
+function createPad(width, height, color) {
+    const pad = new Graphics()
+        .rect(-width / 2, -height / 2, width, height)
+        .fill(color)
+    return pad
+}
+
+createGameParts()
